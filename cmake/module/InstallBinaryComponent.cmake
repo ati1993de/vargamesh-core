ca37342ab2bbf10c080824212e3c5e234fb492ef
Attila Varga <vargamesh@localhost>
2026-09-19 22:25:17 +0000

    doc: generate VargaMesh manpages

--- cmake/module/InstallBinaryComponent.cmake
+++ cmake/module/InstallBinaryComponent.cmake
@@ -23,7 +23,12 @@ function(install_binary_component component)
     COMPONENT ${component}
   )
   if(INSTALL_MAN AND IC_HAS_MANPAGE)
-    install(FILES ${PROJECT_SOURCE_DIR}/doc/man/${target_name}.1
+    set(manpage_name ${target_name})
+    get_target_property(target_output_name ${target_name} OUTPUT_NAME)
+    if(target_output_name)
+      set(manpage_name ${target_output_name})
+    endif()
+    install(FILES ${PROJECT_SOURCE_DIR}/doc/man/${manpage_name}.1
       DESTINATION ${CMAKE_INSTALL_MANDIR}/man1
       COMPONENT ${component}
     )
--- contrib/devtools/gen-manpages.py
+++ contrib/devtools/gen-manpages.py
@@ -10,13 +10,13 @@ import tempfile
 import argparse
 
 BINARIES = [
-'bin/bitcoin',
-'bin/bitcoind',
-'bin/bitcoin-cli',
-'bin/bitcoin-tx',
-'bin/bitcoin-wallet',
-'bin/bitcoin-util',
-'bin/bitcoin-qt',
+'bin/vargamesh',
+'bin/vargameshd',
+'bin/vargamesh-cli',
+'bin/vargamesh-tx',
+'bin/vargamesh-wallet',
+'bin/vargamesh-util',
+'bin/vargamesh-qt',
 ]
 
 parser = argparse.ArgumentParser(
